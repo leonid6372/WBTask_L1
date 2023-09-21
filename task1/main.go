5d4dc4e4742717ab49530d09fe67f7a4db5aa0cc
leonid6372 <94912896+leonid6372@users.noreply.github.com>
2023-09-21 16:45:41 +0300

    Add files via upload

---
+++ task1/main.go
@@ -0,0 +1,37 @@
+// Task 1
+
+package main
+
+import (
+	"fmt"
+)
+
+// Parent struct Human
+type Human struct {
+	Age int
+}
+
+// Parent struct method
+func (h *Human) Greeting() {
+	fmt.Printf("Hello! I'm human %d years old", h.Age)
+}
+
+// Child ctruct
+type Action struct {
+	// Embed parent struct in child
+	Human
+}
+
+func main() {
+	// Create parent struct
+	human := Human{Age: 23}
+
+	// Create child struct
+	action := Action{Human: human}
+
+	// Access parent struct value from child struct
+	if action.Age == 23 {
+		// Access parent struct method from child struct
+		action.Greeting()
+	}
+}
